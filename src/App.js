9f17dd2bb5e701922bb14369020843d2da9fd25a
Griffin Sullivan <gxs331@psu.edu>
2021-10-11 15:23:02 -0400

    setting up routes and nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import './styles/App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home.js';
+import Budget from './pages/Budget.js';
+import Transactions from './pages/Transactions.js';
 
 function App() {
 
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <div>
+        <nav className="nav">
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/budget">Budget</Link>
+          </div>
+          <div>
+            <Link to="/transactions">Transactions</Link>
+          </div>
+        </nav>
+
+        <Switch>
+          <Route path="/budget">
+            <Budget />
+          </Route>
+          <Route path="/transactions">
+            <Transactions />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     
   );
 }
