d0f921e4beec8751cafaf12b5681781af2d6496d
pearman3 <74884284+pearman3@users.noreply.github.com>
2021-11-03 15:45:27 -0400

    styling

--- src/components/LoanCard.js
+++ src/components/LoanCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/LoanCard.css';
 
 const LoanCard = (props) => {
     
     return (
-        <div>
+        <div className= "cards"> 
             <h3>{props.value}</h3>
             <div>{props.title}</div>
             <div>{props.description}</div>
