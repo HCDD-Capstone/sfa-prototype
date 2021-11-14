fadfe05967bf8a3e27229c51f563370c2fbe87d8
Griffin Sullivan <gxs331@psu.edu>
2021-11-14 18:00:29 -0500

    adding functionality to graphs

--- server/models/budget.model.js
+++ server/models/budget.model.js
@@ -1,64 +1,26 @@
 const mongoose = require('mongoose');
 
 const BudgetSchema = mongoose.Schema({
-    "categories":[
-        {
-            "food":
-            {
-                "grocery store": Number,
-                "fast food": Number,
-                "bar": Number
-            }
-        },
-        {
-            "rent":
-            {
-                "payment": Number,
-                "renter's insurance": Number
-            }
-        },
-        {
-            "entertainment":
-            {
-                "TV Subscriptions": Number,
-                "Live Events": Number,
-                "Other": Number
-            }
-        },
-        {
-            "utilities":
-            {
-                "Gas": Number,
-                "water": Number,
-                "Electricity": Number
-            }
-        },
-        {
-            "car":
-            {
-                "insurance": Number,
-                "gas": Number,
-                "repairs": Number
-            }
-        },
-        {
-            "loans":
-            {
-                "mortgage": Number,
-                "Student Loans": Number,
-                "car": Number
-            }
-        },
-        {
-            "other":
-            {
-                "Health Insurance": Number,
-                "Children's Needs": Number,
-                "Clothes": Number
-            }
-        }
-    ],
-    title: String
+    "grocery store": Number,
+    "fast food": Number,
+    "bar": Number,
+    "payment": Number,
+    "renter's insurance": Number,
+    "TV Subscriptions": Number,
+    "Live Events": Number,
+    "Other": Number,
+    "Gas": Number,
+    "water": Number,
+    "Electricity": Number,
+    "insurance": Number,
+    "gas": Number,
+    "repairs": Number,
+    "mortgage": Number,
+    "Student Loans": Number,
+    "car": Number,
+    "Health Insurance": Number,
+    "Children's Needs": Number,
+    "Clothes": Number,
     }, {
     timestamps: false
 });
